8e05b2457fbe4830ca42e681e5bb7e9e9cec3ff0
Vikas Verma <findvikas@gmail.com>
2017-02-05 23:48:52 -0800

    Added content-type and custom user agent

--- IOLED/Http.cpp
+++ IOLED/Http.cpp
@@ -1,12 +1,13 @@
 #include "Http.h"
 #include <ESP8266HTTPClient.h>
 
-
-
+const char *USER_AGENT = "IOLEDv1";
 
 String Http::Get(String url){
 	String retVal;
 	__http.begin(url);
+  __http.setUserAgent(USER_AGENT);
+  __http.addHeader("Content-Type","application/json");
     int httpCode = __http.GET();
     if(httpCode == HTTP_CODE_OK)
         retVal = __http.getString();
@@ -17,6 +18,8 @@ String Http::Get(String url){
 String Http::Post(String url,String body){
 	String retVal;
 	__http.begin(url);
+  __http.setUserAgent(USER_AGENT);
+  __http.addHeader("Content-Type","application/json");
     int httpCode = __http.POST(body);
     if(httpCode == HTTP_CODE_OK)
         retVal = __http.getString();
