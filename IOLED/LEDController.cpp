e472a6af1051e9e9b35e471e723c6a673f343161
Vikas Verma <findvikas@gmail.com>
2017-02-05 23:49:30 -0800

    minimum blink speed 10 milliseconds

--- IOLED/LEDController.cpp
+++ IOLED/LEDController.cpp
@@ -3,7 +3,7 @@
 
 
 #include "LEDController.h"
-const int MIN_LED_INTERVAL      = 100;      // 100 Milliseconds
+const int MIN_LED_INTERVAL      = 10;       // 10 Milliseconds
 const int MAX_LED_INTERVAL      = 60000;    // 60 Seconds
 const int DEFAULT_LED_INTERVAL  = 2000;     // 2 Seconds
 
